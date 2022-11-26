49dfa5a69d1c3d7ffeab374b40a6c6a814ce3002
AMANKUMAR7562 <119098376+AMANKUMAR7562@users.noreply.github.com>
2022-11-26 20:02:38 +0530

    Update int project new.py

--- int project new.py
+++ int project new.py
@@ -1,24 +1,26 @@
-a=int(input("any n number:"))
+from math import sqrt
 b=[]
 c=[]
-for i in range(2,10000):
-    count=0
-    for j in range(1,i+1):
-        if(i%j==0):
-            count+=1
-        else:
-            continue
-    if(count<=2):
-        b.append(i)
-for k in b:
+for k in range(2,10000000):
     m=k
     reversed_num=0
     while k!=0:
-        digit=k % 10
+        digit = k % 10
         reversed_num = reversed_num * 10 + digit
-        k//=10
+        k//= 10
     if(m==reversed_num):
-        c.append(m)
-    else:
-        continue
-print(c[a-1])
+        b.append(m)
+  
+ 
+for i in b:
+    t=0
+    if(i>1):
+        for j in range(2,int(sqrt(i))+1):
+            if(i%j==0):
+                t=1
+                break
+        if(t==0):
+            c.append(i)
+    
+n=int(input("enter the number to find the nth prime palindrome:"))
+print(c[n-1])
